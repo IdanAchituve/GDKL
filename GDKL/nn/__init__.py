1d05860caff7c6fb35b53e36315f299900714591
root <root@com120226.localdomain>
2023-02-17 12:33:47 +0200

    delete DKL trainers

--- GDKL/nn/__init__.py
+++
@@ -1,7 +0,0 @@
-from GDKL.nn.spectral_batchnorm import (
-    SpectralBatchNorm1d,
-    SpectralBatchNorm2d,
-    SpectralBatchNorm3d,
-)
-from GDKL.nn.spectral_norm_conv import spectral_norm_conv
-from GDKL.nn.spectral_norm_fc import spectral_norm_fc
